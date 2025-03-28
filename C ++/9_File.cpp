0a42016ecd460488909206b640ef46db8a64eba7
AherVaishnavi <139205101+AherVaishnavi@users.noreply.github.com>
2025-03-29 02:00:23 +0530

    Update 9_File.cpp

--- C ++/9_File.cpp
+++ C ++/9_File.cpp
@@ -1,3 +1,11 @@
+/* Write a function in C++ to count and display the number of lines not starting with alphabet 'A' present in a text file "STORY.TXT".
+   Example:
+      If the file "STORY.TXT" contains the following lines, The roses are red.
+      A girl is playing there. There is a playground.
+      An aeroplane is in the sky.
+      Numbers are not allowed in the password. T
+      he function should display the output as 3. */
+
 #include<iostream.h>
 #include<conio.h>
 #include<fstream.h>
