56227b9549cb15c9abd3e6196546309589390fcd
AherVaishnavi <139205101+AherVaishnavi@users.noreply.github.com>
2025-03-29 01:51:08 +0530

    Update 2_Quadratic.cpp

--- C ++/2_Quadratic.cpp
+++ C ++/2_Quadratic.cpp
@@ -1,3 +1,11 @@
+/* Implement a class Quadratic that represents degree two polynomials i.e., polynomials of type
+   ax2+bx+c. The class will require three data members corresponding to a, b and c. Implement the following operations:
+   1. A constructor (including a default constructor which creates the 0polynomial).
+   2. Overloaded operator+ to add two polynomials of degree2.
+   3. Overloaded << and >> to print and read polynomials. To do this, you will need to decide what you want your input and output format to looklike.
+   4. A function eval that computes the value of a polynomial for a given value ofx.
+   5. A function that computes the two solutions of the equationax2+bx+c=0.*/
+
 #include<iostream.h>
 #include<conio.h>
 
