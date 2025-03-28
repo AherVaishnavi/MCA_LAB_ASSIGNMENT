193d90bfb6ffeb2e3642308a94dd5a93f29b2d7e
AherVaishnavi <139205101+AherVaishnavi@users.noreply.github.com>
2025-03-29 01:57:29 +0530

    Update 7_Rational.cpp

--- C ++/7_Rational.cpp
+++ C ++/7_Rational.cpp
@@ -1,3 +1,8 @@
+/* Create a class Rational Number (fractions) with the following capabilities:
+    a) Create a constructor that prevents a 0 denominator in a fraction, reduces or simplifies fractions that are not in reduced form and avoids negative denominators.
+    b) Overload the addition, subtraction, multiplication and division operators for this class.
+    c) Overload the relational and equality operators for this class. */
+
 #include<iostream.h>
 #include<conio.h>
 class a
